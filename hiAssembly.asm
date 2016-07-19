7a225dc70a6ea0955ee34629c33e99c976be22a2
Jozadaque Batista <jozadaquebatista@gmail.com>
2016-07-19 00:20:43 -0300

    update hiassembly.asm
    just some great big awesome and majestic, update to code! :D

--- hiAssembly.asm
+++ hiAssembly.asm
@@ -1,20 +1,26 @@
-section .data   ; sessao que que trata das var armazenadas
-msg db 'Hello, I'm assembly', 0AH
-len equ $-msg
+; nasm -s -felf32 hiassembly.asm
+; ld -s -melf_i386 -o hiassembly hiassembly.o
+; ./hiassembly
 
-section .bss
-var_qualquer db 4
+section .data                     ; deals with initialized data
+msg db 'Hello, I'm assembly', 0AH ; message I want to write on the screen
+len equ $-msg                     ; var length
 
-section .text   ; sessao que fica armazenadas as intrucoes
-global _start
+section .bss                      ; unitiallized data
+var_qualquer db 4                 ; just declare an unitiallized var
+
+section .text                     ; all src code will be wrote here
+global _start                     ; defines start of program
 _start:
-
-        mov edx, len
-        mov ecx, msg
-        mov ebx, 1
-        mov eax, 4
-        int 80h
-
-        mov ebx, 0
-        mov eax, 1
-        int 80h
+  ; WRITE
+  mov edx, len                    ; pass the length of msg in edx register   
+  mov ecx, msg                    ; pass the msg in ecx register
+  mov ebx, 1                      ; pass to ebx, the file descriptor nmbr
+  mov eax, 4                      ; syscall write nmbr
+  int 80h                         ; interrupt that calls the kernel env
+  ; EXIT
+  mov ebx, 0                      ; return value of exit
+  mov eax, 1                      ; pass to ebx, the nmbr of exit syscall
+  int 80h                         ; interrupt that calls the kernel to do something
+  
+  ; ALL RIGHT, that's all folks! õ/ I-Y-L
