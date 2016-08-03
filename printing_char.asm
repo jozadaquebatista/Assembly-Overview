9b5e6042f9b43d0b6a2c83d112c28de9d42ea6aa
Jozadaque Batista <jozadaquebatista@gmail.com>
2016-08-02 23:48:45 -0300

    Create printing_char.asm

---
+++ printing_char.asm
@@ -0,0 +1,31 @@
+[bits 32]
+
+%define syscall 0x80
+
+%macro print 2
+        mov eax,0x04      ; sys_write      
+        mov ebx,0x01      ; fd
+        
+        push 'A'
+        mov ecx, esp      ; esp *---> 'A'
+        mov edx,0x01      ; len char
+        int syscall
+        add esp,0x04      ; restore esp
+%endmacro
+
+%macro exit 1
+        mov eax,0x01      ; sys_exit            
+        mov ebx,%1            
+        int syscall
+%endmacro
+
+segment .rodata
+segment .data
+segment .bss
+
+  section .text
+        global _start
+
+  _start:
+        write 'J'
+        exit 0
